8f66ace333e05e5589fd29ee17b32fab911711c5
LeandroSinich <leandrosinich@gmail.com>
2024-07-01 11:44:13 -0300

    formulario de contacto

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  
+  formulario!: FormGroup;
+
+  constructor(private formbuilder: FormBuilder){
+    this.formulario = this.formbuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      mensaje: ['', [Validators.required, Validators.minLength(10)]]
+    })
+  }
+  
+  ngOnInit(): void {
+    
+  }
+
+  enviar(event: Event){
+    event.preventDefault();
+    console.log(this.formulario);
+  }
+  hasError(field: string, typeError: string){
+    return this.formulario.get(field)?.hasError(typeError) && this.formulario.get(field)?.touched;
+  }
 
 }
